dac494258d11ae81acd4af22ec78d4caeaad992e
pawnsir <pawnsir@localhost.localdomain>
2019-07-29 19:50:49 +0800

    add bubble sort

--- Introduction to Algorithms/SourceCode/C++/src/Sort/main.cpp
+++ Introduction to Algorithms/SourceCode/C++/src/Sort/main.cpp
@@ -4,6 +4,7 @@ int main()
 {
         int a[] = {5,4,3,2,1};
         //Logger(InsertSort(a,sizeof(a)/sizeof(int)),5);
-        Logger(MergeSort(a,sizeof(a)/sizeof(int)),5);
+        //Logger(MergeSort(a,sizeof(a)/sizeof(int)),5);
+        Logger(BubbleSort(a,sizeof(a)/sizeof(int)),5);
         return 0;
 }
