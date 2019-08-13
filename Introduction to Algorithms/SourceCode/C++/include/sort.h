1a54f6c2ec34f3f447bd6bfcc8fecb9a333889ec
Pawnsir <pawnsir@163.com>
2019-08-13 10:32:29 +0800

    heap test

--- Introduction to Algorithms/SourceCode/C++/include/sort.h
+++ Introduction to Algorithms/SourceCode/C++/include/sort.h
@@ -4,6 +4,6 @@
 int * InsertSort(int *arr, int len);
 int * MergeSort(int *arr, int len);
 int * BubbleSort(int *arr, int len);
-int * HeapSort(int *arr, int len);
+int * BuildHeap(int *arr, int len);
 
 #endif
--- Introduction to Algorithms/SourceCode/C++/src/Sort/HeapSort.cpp
+++ Introduction to Algorithms/SourceCode/C++/src/Sort/HeapSort.cpp
@@ -1,21 +1,28 @@
 #include "sort.h"
 #include <stdio.h>
 #include <assert.h>
-
+#include <iostream>
 int* BuildHeap(int* arr, int len)
 {
     assert(arr);
     assert(len > 1);
-    int left = len - 2;
-    int right = len - 1;
-    int father = right / 2 - 1;
-    do {
-        int max = arr[left] > arr[right] ? left : right;
-        if(arr[father] < arr[max])
+    int child = len - 1;
+    int father = len - 1;
+    int begin = len - 1;
+    while(begin >= 0)
+    {
+        child = (begin+1) / 2;
+        while(child > 0)
         {
-            std::swap(arr[father], arr[max]);
+            int min = arr[child] < arr[child-1] ? child : child-1;
+            if(arr[father] > arr[min])
+            {
+                std::swap(arr[father], arr[min]);
+            }
+            child = (child / 2) - 1;
         }
+        begin--;
         
-    } while(father != 0);
+    }
     return arr;
 }
