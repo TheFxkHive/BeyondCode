12823c8b45b4b7d4989e48e6b4f902ae86c6020a
pawnsir <pawnsir@localhost.localdomain>
2019-08-13 22:07:25 +0800

    fix build heap

--- Introduction to Algorithms/SourceCode/C++/src/Sort/HeapSort.cpp
+++ Introduction to Algorithms/SourceCode/C++/src/Sort/HeapSort.cpp
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include "log.h"
 #include <stdio.h>
 #include <assert.h>
 #include <iostream>
@@ -6,23 +7,31 @@ int* BuildHeap(int* arr, int len)
 {
     assert(arr);
     assert(len > 1);
-    int child = len - 1;
-    int father = len - 1;
-    int begin = len - 1;
+    int child = 0;
+    int father = 0;
+    int begin = (len - 2) / 2;
     while(begin >= 0)
     {
-        child = (begin+1) / 2;
-        while(child > 0)
+        father = begin;
+        child = father * 2 + 1;
+        while(child < len)
         {
-            int min = arr[child] < arr[child-1] ? child : child-1;
+            int min = child;
+            if (child + 1 < len)
+                min = arr[child] < arr[child+1] ? child : child+1;
             if(arr[father] > arr[min])
             {
                 std::swap(arr[father], arr[min]);
             }
-            child = (child / 2) - 1;
+            //if(father == 0)
+            //{
+            //    break;
+            //}
+            father = child;
+            child = father * 2 + 1;
         }
         begin--;
-        
+        Logger(arr,5);
     }
     return arr;
 }
